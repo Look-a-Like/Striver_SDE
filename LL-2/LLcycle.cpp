e3f88178fd047872e395094eda1d41920eac393d
Look-a-Like <dheerangude@gmail.com>
2025-02-07 02:24:19 +0530

    LL-2

---
+++ LL-2/LLcycle.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast != NULL && fast->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (slow == fast)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+void insert(ListNode *&head, int x)
+{
+    if (head == NULL)
+    {
+        head = new ListNode(x);
+        return;
+    }
+    ListNode *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = new ListNode(x);
+}
+
+void print(ListNode *head)
+{
+    ListNode *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int x;
+    ListNode *head = NULL;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+        insert(head, x);
+    }
+    cin >> x;
+    print(head);
+    ListNode *temp = head;
+    while (temp->val != x)
+    {
+        temp = temp->next;
+    }
+    Solution object;
+    bool ans = object.hasCycle(temp);
+    cout << ans;
+}
